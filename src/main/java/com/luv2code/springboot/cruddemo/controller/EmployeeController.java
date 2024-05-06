39058bdb0d60d02d4e7bfbe7bba7d670134f1594
Pawel <p380623@gmail.com>
2024-05-06 19:07:17 +0200

    #4

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,16 @@ public class EmployeeController {
     @GetMapping("/showFormForAdd")
     public String showFormForAdd(Model model){
         Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+
+        Employee employee = employeeService.findById(id);
 
         model.addAttribute("employee", employee);
 
